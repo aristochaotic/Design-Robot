5ba51da9fb581d22b67848facdde019837bb75e7
Tyler Wright <tw39440@gmail.com>
2018-09-25 08:46:37 -0400

    added Arduino files

---
+++ Robot/Robot.ino
@@ -0,0 +1,17 @@
+#include WemosInit
+#include Servo
+
+Servo left;
+Servo right;
+
+
+void setup() {
+  // adjust later for actual pins
+  left.attach(R1);
+  right.attach(R2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
